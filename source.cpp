30fab6b97473e7a3837b4eb3504238de53cf08f3
BayramAlpSahin <94050498+BayramAlpSahin@users.noreply.github.com>
2021-11-21 01:50:51 +0300

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,9 +1,109 @@
-//C++ varsayılan ayarda Türkçe karakter yazdırmıyor. Destekleyen standart kütüphaneyi eklememiz gerekiyor.
+#include <bits/stdc++.h>
+#include <locale.h>
+using namespace std;
+ 
 
-#include <locale.h> // veya C++ için <clocale>
-int main() {
-  setlocale(LC_ALL, "Turkish");
-  printf("Türkçe karakter yazdırmak için örnek kod\n");
-  printf("setlocale fonksiyonu olmadan çalıştırınca ekrana saçmasapan bir yazı bastırır.\n");
-  return 0;
+int main()
+{ 	setlocale(LC_ALL, "Turkish");
+	int x;
+	cin>>x;
+	switch (x/100)
+	{
+	case 1:
+		cout<<"yüz";
+		break;
+	case 2:
+		cout<<"ikiyüz";
+		break;
+	case 3:
+		cout<<"üçyüz";
+		break;
+	case 4:
+		cout<<"dörtyüz";
+		break;
+	case 5:
+		cout<<"beşyüz";
+		break;
+	case 6:
+		cout<<"altıyüz";
+		break;
+	case 7:
+		cout<<"yediyüz";
+		break;
+	case 8:
+		cout<<"sekizyüz";
+		break;	
+	case 9:
+		cout<<"dokuzyüz";
+		break;
+	default:
+		break;}
+	x-=x/100*100;
+	switch (x/10)
+	{
+	case 1:
+		cout<<"on";
+		break;
+	case 2:
+		cout<<"yirmi";
+		break;
+	case 3:
+		cout<<"otuz";
+		break;
+	case 4:
+		cout<<"kırk";
+		break;
+	case 5:
+		cout<<"elli";
+		break;
+	case 6:
+		cout<<"otuz";
+		break;
+	case 7:
+		cout<<"yetmiş";
+		break;
+	case 8:
+		cout<<"seksen";
+		break;	
+	case 9:
+		cout<<"doksan";
+		break;
+	default:
+		break;}
+	x-=x/10*10;
+	switch (x)
+	{
+	case 1:
+		cout<<"bir";
+		break;
+	case 2:
+		cout<<"iki";
+		break;
+	case 3:
+		cout<<"üç";
+		break;
+	case 4:
+		cout<<"dört";
+		break;
+	case 5:
+		cout<<"beş";
+		break;
+	case 6:
+		cout<<"altı";
+		break;
+	case 7:
+		cout<<"yedi";
+		break;
+	case 8:
+		cout<<"sekiz";
+		break;	
+	case 9:
+		cout<<"dokuz";
+		break;
+	default:
+		break;}
+	
+		
+	
+return 0;
 }
